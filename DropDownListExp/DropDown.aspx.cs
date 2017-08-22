381fd30aa9b99275329ea557bed002bcc61a671b
RostyslavZhovnir <zhovnir.info@gmail.com>
2017-08-21 23:02:58 -0500

    Comments Added

--- DropDownListExp/DropDown.aspx.cs
+++ DropDownListExp/DropDown.aspx.cs
@@ -18,17 +18,17 @@ namespace DropDownListExp
             {
              ddlOptions.DataSource = GetData("spGetOption", null);
              ddlOptions.DataBind();
-                ListItem liOption = new ListItem("Select Option", "-1");
+                ListItem liOption = new ListItem("Select Option", "-1");  //Default Value for DDL liOption
                 ddlOptions.Items.Insert(0, liOption);
 
-                ListItem liPolicy = new ListItem("Select Policy", "-1");
+                ListItem liPolicy = new ListItem("Select Policy", "-1");  //Default Value for DDL liPolicy
                 ddlPolicy.Items.Insert(0, liPolicy);
 
                 ddlPolicy.Enabled = false;
 
             }
         }
-
+        //Universal Method to Set Connection String and work with SQL Procedures which was made in SQL (spGetOption,@OptionId)
         private DataSet GetData(string SpName, SqlParameter SpParameter)
         {
             string ConnString = ConfigurationManager.ConnectionStrings["ConnStringTestDb"].ConnectionString;
@@ -36,7 +36,7 @@ namespace DropDownListExp
             SqlDataAdapter da = new SqlDataAdapter(SpName, con);
             da.SelectCommand.CommandType = CommandType.StoredProcedure;
 
-            if (SpParameter != null)
+            if (SpParameter != null) //Procedure spGetOption do not have second parameter, We need to check it for null
             {
                 da.SelectCommand.Parameters.Add(SpParameter);
             }
