c0b8945d2fc96478f838adc4375c9b919e8a93e4
RostyslavZhovnir <zhovnir.info@gmail.com>
2017-08-21 22:25:14 -0500

    WebFormCreated

---
+++ DropDownListExp/DropDown.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="DropDown.aspx.cs" Inherits="DropDownListExp.DropDown" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:DropDownList ID="ddlOptions" width="200px" runat="server" AutoPostBack="true"
+         DataTextField ="Name" DataValueField="ID" OnSelectedIndexChanged="ddlOptions_SelectedIndexChanged">
+        </asp:DropDownList>
+        <br />
+        <br />
+        <br />
+        <asp:DropDownList ID="ddlPolicy" width="200px" runat="server" DataTextField="PolicyName" DataValueField="PolicyID">
+        </asp:DropDownList>
+    </form>
+</body>
+</html>
---
+++ DropDownListExp/DropDown.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace DropDownListExp
+{
+    public partial class DropDown : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+             ddlOptions.DataSource = GetData("spGetOption", null);
+             ddlOptions.DataBind();
+                ListItem liOption = new ListItem("Select Option", "-1");
+                ddlOptions.Items.Insert(0, liOption);
+
+                ListItem liPolicy = new ListItem("Select Policy", "-1");
+                ddlPolicy.Items.Insert(0, liPolicy);
+
+                ddlPolicy.Enabled = false;
+
+            }
+        }
+
+        private DataSet GetData(string SpName, SqlParameter SpParameter)
+        {
+            string ConnString = ConfigurationManager.ConnectionStrings["ConnStringTestDb"].ConnectionString;
+            SqlConnection con = new SqlConnection(ConnString);
+            SqlDataAdapter da = new SqlDataAdapter(SpName, con);
+            da.SelectCommand.CommandType = CommandType.StoredProcedure;
+
+            if (SpParameter != null)
+            {
+                da.SelectCommand.Parameters.Add(SpParameter);
+            }
+            DataSet DS = new DataSet();
+            da.Fill(DS);
+            return DS;
+
+        }
+
+        protected void ddlOptions_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (ddlOptions.SelectedIndex !=0)
+            {
+                ddlPolicy.Enabled = true;
+                SqlParameter parameter = new SqlParameter("@OptionId", ddlOptions.SelectedValue);
+                DataSet DS =GetData("spGetPolicyByOptionId ", parameter);
+                ddlPolicy.DataSource = DS;
+                ddlPolicy.DataBind();
+                ListItem liPolicy = new ListItem("Select Policy", "-1");
+                ddlPolicy.Items.Insert(0, liPolicy);
+            }
+        }
+    }
+}
